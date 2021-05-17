4e83e7acaac91216e027e3409ba8d7219b18c50b
lwm886 <227959626@qq.com>
2021-05-17 18:03:15 +0800

    分页插件集成

--- src/main/java/com/tech/datasource/user/controller/UserController.java
+++ src/main/java/com/tech/datasource/user/controller/UserController.java
@@ -1,14 +1,12 @@
 package com.tech.datasource.user.controller;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.tech.datasource.user.entity.User;
 import com.tech.datasource.user.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -19,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-05-17
  */
 @RestController
-@RequestMapping("/user/user")
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
@@ -28,5 +26,13 @@ public class UserController {
     @PostMapping("save")
     void save(@RequestBody User user){
         userService.save(user);
+    }
+
+    @GetMapping("p")
+    IPage<User> pages(){
+        IPage<User> page=new Page<>();
+        page.setPages(1);
+        page.setSize(3);
+        return userService.page(page);
     }
 }
---
+++ src/main/java/com/tech/datasource/utils/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.tech.datasource.utils;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 分页插件
+ * @author lw
+ * @since 2021/5/17
+ **/
+@Configuration
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
